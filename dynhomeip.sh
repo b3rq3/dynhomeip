e6cc129724d12ac4b99a7c1237aef09ba60ce9fe
berq <bq@yuki.(none)>
2013-08-25 10:07:25 +0200

    working now

--- dynhomeip.sh
+++ dynhomeip.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 #
 #  place this script in ~/.dynhomeip
-#  and generate a cronjob e.g. for every 5 minutes to check your ip-address
-#  well dyndns changed it's service, but we don't need it
-#  this script uses curl, egrep to point to your dynamic ip address
-#  the only thing you need is a webspace to upload your redirect.html
+#  and generate a cronjob e.g. for every 10 minutes to check your ip-address
+#  create also a ftp.login file with your ftp credentials and ftp-server in ~/.dynhomeip 
+#  the only thing you need is a webspace to upload the redirect.html file
 #  have fun
 
-FTPUSER=""
-FTPPWD=""
-FTPSERVER=""
+FTPLOGIN=`awk 'NR==1' ftp.login`
+FTPSERVER=`awk 'NR==2' ftp.login`
 
 # \o/ duckduckgo - let's get our current ip address
 LIVEIP=`curl -s https://duckduckgo.com/?q=my+ip | egrep -o '([[:digit:]]{1,3}\.){3}[[:digit:]]{1,3}'`
@@ -20,18 +18,21 @@ fi
 
 MYIP=`head -1 myip`
 
-if [ $LIVEIP!=$MYIP ]; then 
+if [ "$LIVEIP" != "$MYIP" ]; then 
 	# ip changed write to myip
         echo $LIVEIP >myip
-	# read new ip and upload a html script with a forwarder to your new ip
+	# read new ip and upload a html file with a forwarder to your new ip
 	NEWIP=`head -1 myip`
 	printf -v HTML %s '<html>\n<head>\n<meta http-equiv="refresh" content="0; URL=http://' ${NEWIP} '">\n</head>\n</html>';
 	echo -e $HTML >redirect.html
 
 	# upload redirect.html file to your webspace
-	#curl --ssl-reqd --ftp-ssl-ccc -u $FTPUSER:$FTPWD ftp://$FTPSERVER/ -v -k -T redirect.html
+	# -k means there is no certificate check, this can be insecure
+	curl -s -S --ssl-reqd -u $FTPLOGIN ftp://$FTPSERVER -k -T redirect.html
 
-	# delete redirect.html
+	# if your ftp doesn't support tls use this 
+	# curl -s -S -u $FTPLOGIN ftp://$FTPSERVER -T redirect.html
+
 	#rm -f redirect.html
 fi
 
